dd431d8e365a6dbef1331d45172f35822a7998a4
jonasarce1 <jonasarceg@gmail.com>
2023-11-30 17:35:13 +0100

    cambio populate

--- resolvers/getTrabajadores.ts
+++ resolvers/getTrabajadores.ts
@@ -6,8 +6,12 @@ import { TrabajadorModel, TrabajadorModelType } from "../db/trabajador.ts";
 
 export const getTrabajadores = async(_req:Request, res:Response<Array<TrabajadorModelType> | {error : unknown}>) => {
     try{
-        const trabajadores = await TrabajadorModel.find({}).populate("empresa").populate("tareas").exec(); //Populate para que se vea la empresa y las tareas
-        
+        const trabajadores = await TrabajadorModel.find({})
+        .populate({path: "empresa", select: "nombre tipo"})
+        .populate({path: "tareas", select: "nombre estado"})
+        .exec(); //Populate para que se vea la empresa y las tareas        
+
+
         res.status(200).send(trabajadores);
     }catch(error){
         if(error instanceof mongoose.Error.ValidationError){ //si el error es del modelo de mongoose
