98e79285ce26212cdfa5c996c9e48b63a6aa7a32
jonasarce1 <jonasarceg@gmail.com>
2023-11-30 16:04:37 +0100

    cambio getEmpresa populate

--- resolvers/getEmpresa.ts
+++ resolvers/getEmpresa.ts
@@ -8,14 +8,14 @@ export const getEmpresa = async(req:Request<{id:string}>, res:Response<EmpresaMo
     try{
         const id = req.params.id;
 
-        const empresa = await EmpresaModel.findById(id).exec();
+        const empresa = await EmpresaModel.findById(id).populate("trabajadores").exec(); //Populate para que se vean los trabajadores
 
         if(!empresa){
             res.status(404).send("No se pudo encontrar la empresa");
             return;
         }
 
-        res.status(200).send(empresa.populate("trabajadores")); //Populate para que se vean los trabajadores
+        res.status(200).send(empresa); 
     }catch(error){
         if(error instanceof mongoose.Error.ValidationError){ //si el error es del modelo de mongoose
             res.status(400).send(error); 
