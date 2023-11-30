4c6aac5354eb272a2cd470a3bb3ad88a96628b30
jonasarce1 <jonasarceg@gmail.com>
2023-11-30 15:40:27 +0100

    comentado deno exit

--- main.ts
+++ main.ts
@@ -10,10 +10,10 @@ const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
   console.log("No mongo URL found");
-  Deno.exit(1);
+  //Deno.exit(1);
 }
 
-await mongoose.connect(MONGO_URL);
+//await mongoose.connect(MONGO_URL);
 
 const app = express(); 
 app.use(express.json());
