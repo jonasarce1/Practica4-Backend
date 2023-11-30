6c3708bdfa766362b0dec74b7a16fc40f80dbfe2
jonasarce1 <jonasarceg@gmail.com>
2023-11-30 11:22:39 +0100

    quitado deno exit, luego poner

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
   console.log("No mongo URL found");
-  Deno.exit(1);
+  //Deno.exit(1);
 }
 
-await mongoose.connect(MONGO_URL);
+//await mongoose.connect(MONGO_URL);
