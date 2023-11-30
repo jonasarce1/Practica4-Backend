5c1b4f206f4a683a90deaea42d0710e7623c9e6f
jonasarce1 <jonasarceg@gmail.com>
2023-11-30 16:29:48 +0100

    cambios populate getTrabajadores

--- resolvers/getTrabajadores.ts
+++ resolvers/getTrabajadores.ts
@@ -6,7 +6,7 @@ import { TrabajadorModel, TrabajadorModelType } from "../db/trabajador.ts";
 
 export const getTrabajadores = async(_req:Request, res:Response<Array<TrabajadorModelType> | {error : unknown}>) => {
     try{
-        const trabajadores = await TrabajadorModel.find({}).exec();
+        const trabajadores = await TrabajadorModel.find({}).populate("empresa").populate("tareas").exec(); //Populate para que se vea la empresa y las tareas
         
         res.status(200).send(trabajadores);
     }catch(error){
